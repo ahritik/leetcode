2656305d3f95c9bdd71e8d1824a99e95ea806a3e
Hritik Arasu <hritikarasu@gmail.com>
2022-06-04 21:24:20 -0700

    SeparateFiles1

---
+++ CanPlaceFlowers.py
@@ -0,0 +1,26 @@
+'''
+605. Can Place Flowers
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, 
+return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+'''
+
+def canPlaceFlowers(flowerbed, n: int) -> bool:
+    if n == 0: 
+        return True
+    flowerbed = [0] + flowerbed + [0]
+    
+    for i in range(1, len(flowerbed)-1):
+        if flowerbed[i] == 1:
+            continue
+        elif flowerbed[i - 1] == flowerbed[i + 1] == 0:
+            n-=1
+            flowerbed[i] = 1
+            if n == 0:
+                return True
+    
+    return False
+
+print("Input: flowerbed = [1,0,0,0,1], n = 1 Output:",canPlaceFlowers([1,0,0,0,1], 1))
+print("Input: flowerbed = [1,0,0,0,1], n = 2 Output:",canPlaceFlowers([1,0,0,0,1], 2))
+print("Input: flowerbed = [], n = 2          Output:",canPlaceFlowers([], 2))
---
+++ PalindromeNumber9.py
@@ -0,0 +1,25 @@
+'''
+9. Palindrome Number
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+'''
+
+def isPalindrome(x: int) -> bool:
+    if x < 0:
+        return False
+
+    reverse = 0
+    temp = x
+
+    while temp > 0:
+        reverse = reverse * 10 + temp % 10
+        temp //= 10
+
+    return reverse == x
+
+print("Input: 121  Output:",isPalindrome(121))
+print("Input: -121 Output:",isPalindrome(-121))
+print("Input: 10   Output:",isPalindrome(10))
